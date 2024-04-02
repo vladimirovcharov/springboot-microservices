82e36296dcf49cc1ce982e048c30f88c532070b5
Volodymyr Ovcharov <vladimir.1527@gmail.com>
2024-04-02 18:30:59 -0500

    Add docker deployment for all components

--- department-service/src/main/java/org/example/departmentservice/controller/DepartmentController.java
+++ department-service/src/main/java/org/example/departmentservice/controller/DepartmentController.java
@@ -6,7 +6,6 @@ import org.example.departmentservice.model.Department;
 import org.example.departmentservice.repository.DepartmentRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
--- employee-service/src/main/java/org/example/employeeservice/controller/EmployeeController.java
+++ employee-service/src/main/java/org/example/employeeservice/controller/EmployeeController.java
@@ -5,7 +5,6 @@ import org.example.employeeservice.model.Employee;
 import org.example.employeeservice.repository.EmployeeRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
