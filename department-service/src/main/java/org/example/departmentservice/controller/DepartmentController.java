557e970964b1b73556072597f8cc81bd4bc6e991
Volodymyr Ovcharov <vladimir.1527@gmail.com>
2024-04-09 19:41:04 -0500

    Add implementation of circuit breaker

--- department-service/src/main/java/org/example/departmentservice/controller/DepartmentController.java
+++ department-service/src/main/java/org/example/departmentservice/controller/DepartmentController.java
@@ -1,18 +1,17 @@
 package org.example.departmentservice.controller;
 
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import lombok.AllArgsConstructor;
 import org.example.departmentservice.client.EmployeeClient;
 import org.example.departmentservice.model.Department;
 import org.example.departmentservice.repository.DepartmentRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.Collections;
 import java.util.List;
 
 @RestController
@@ -43,11 +42,16 @@ public class DepartmentController {
     }
 
     @GetMapping("/with-employees")
-    public List<Department> findAllWithEmployees() {
+    @CircuitBreaker(name = "employee-service", fallbackMethod = "getEmptyDepartments")
+    public ResponseEntity<List<Department>> findAllWithEmployees() {
         LOGGER.info("Department find all with employees");
         List<Department> departments = repository.findAll();
         departments.forEach(department -> department.setEmployees(employeeClient.findByDepartment(department.getId())));
-        return departments;
+        return new ResponseEntity<>(departments, HttpStatus.OK);
     }
 
+    public ResponseEntity<List<Department>> getEmptyDepartments(Exception e) {
+        LOGGER.error("Oops! Something went wrong! Try again later. {}", e.getMessage());
+        return new ResponseEntity<>(Collections.emptyList(), HttpStatus.SERVICE_UNAVAILABLE);
+    }
 }
